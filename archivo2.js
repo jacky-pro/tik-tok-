48b46d135db5b864dfb1ef8d2dbb7947b35a242a
Jackydata <jklnuag08@gmail.com>
2024-12-26 23:19:17 -0500

    comunidad.ipynb

--- archivo2.js
+++ archivo2.js
@@ -1,1 +1,5 @@
-print("LA MEJOR COMUNIDAD ES DE JACKYDATA")
+// Crear una lista (array) de cadenas
+let lista = ["Hola", "Mundo", "JavaScript", "es", "genial"];
+
+// Mostrar la lista completa
+console.log("Lista completa:", lista);
